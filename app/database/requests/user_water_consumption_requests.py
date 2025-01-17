83db9dd2fb832ef9a9717b51f3e0e37f0c4e2629
Ломакин Павел Николаевич <okoksnedam@mail.ru>
2025-01-17 22:40:40 +0300

    get amount of water drunk for selected day

--- app/database/requests/user_water_consumption_requests.py
+++ app/database/requests/user_water_consumption_requests.py
@@ -1,10 +1,21 @@
+from sqlalchemy import select, extract
+
 from app.database.models import async_session
-from sqlalchemy import select, inspect
 from app.database.models import UserWaterConsumption
 from datetime import datetime
+from sqlalchemy.sql import func
 
 
-async def set_water_consumption(tg_id: int, n_liters: float, date: datetime):
+async def set_water_consumption(tg_id: int, n_liters: float, date: datetime) -> None:
     async with async_session() as session:
         session.add(UserWaterConsumption(tg_id=tg_id, n_liters=n_liters, date=date))
         await session.commit()
+
+
+async def get_total_water_consumption(tg_id: int, date: datetime) -> float:
+    async with async_session() as session:
+        return await session.scalars(select(func.sum(UserWaterConsumption.n_liters))
+                                     .where(UserWaterConsumption.tg_id == tg_id)
+                                     .filter(extract('year', UserWaterConsumption.date) == date.year)
+                                     .filter(extract('month', UserWaterConsumption.date) == date.month)
+                                     .filter(extract('day', UserWaterConsumption.date) == date.day))
