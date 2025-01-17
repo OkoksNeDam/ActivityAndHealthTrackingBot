2bda3b35673213e63d677531dc6cdef7d2405a33
Ломакин Павел Николаевич <okoksnedam@mail.ru>
2025-01-17 20:42:36 +0300

    deleted registration state

--- app/states/states.py
+++ app/states/states.py
@@ -1,7 +1,4 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class Registration(StatesGroup):
-    choosing_first_name = State()
-    choosing_last_name = State()
-    choosing_favorite_animal = State()
+
