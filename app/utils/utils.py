607297f3aa4a0f7dfb6228b83f8eff7613ad80bf
Ломакин Павел Николаевич <okoksnedam@mail.ru>
2025-01-18 14:13:54 +0300

    created func that get product calories

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,7 +1,8 @@
 import requests
 from app.config import WEATHER_API_KEY
 
-WEATHER_API_URL = 'http://api.weatherapi.com/v1/current.json'
+WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
+FOOD_INFO_API_URL = "https://world.openfoodfacts.org/cgi/search.pl"
 
 
 def get_temperature_in(city: str) -> float:
@@ -18,3 +19,18 @@ def calc_water_intake(weight: float, activity_level: float, city: str):
     required_water_amount = weight * 30 + 500 * activity_level / 30 + (500 if temperature > 25 else 0)
     required_water_amount /= 1000
     return required_water_amount
+
+
+def get_calories_of(product_name: str) -> float:
+    response = requests.get(FOOD_INFO_API_URL, params={
+        'action': 'process',
+        'search_terms': product_name,
+        'json': 'true'
+    })
+    if response.status_code == 200:
+        data = response.json()
+        products = data.get('products', [])
+        if products:  # Проверяем, есть ли найденные продукты
+            first_product = products[0]
+            return first_product.get('nutriments', {}).get('energy-kcal_100g', 0)
+    print(f"Ошибка: {response.status_code}")
